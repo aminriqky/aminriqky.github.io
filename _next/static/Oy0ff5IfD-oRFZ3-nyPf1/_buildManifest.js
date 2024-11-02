1535cbc1ff08526a412db0779ab0a8664b0507f7
aminriqky <aminriqky@gmail.com>
2024-11-02 12:23:26 +0700

    new menu

--- _next/static/Oy0ff5IfD-oRFZ3-nyPf1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/490-ca060dc65be4cbd2.js","static/chunks/pages/index-ddd2cf5d20c01330.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
